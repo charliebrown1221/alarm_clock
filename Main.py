f6288fe1091ba31803550f384556e0e7eb6b4899
charliebrown1221 <chaimbrown1@yahoo.com>
2022-05-11 16:49:13 -0400

    finished project might add stuff

---
+++ Main.py
@@ -0,0 +1,6 @@
+from alarm_clock import Alarmclock
+
+time= Alarmclock()
+time.set_time()
+time.set_alarm_time()
+time.alarm_set()
--- alarm_clock.py
+++ alarm_clock.py
@@ -1,35 +1,33 @@
-class Alarm_clock:
+class Alarmclock:
 
     def __init__(self):
         self.time_now = ''
         self.alarm_time =''
-        self.alarm_on = True
+        self.alarm_on = False
         
 
-def set_time(self):  
-    current_time = input('Current time is 1000 do yo want to change it? Enter y/n:  ')  
-    self.time_now= current_time
-    if current_time == 'y':
-        current_time = input ('What time is it? ')
-    else:
-        current_time = 1000    
+    def set_time(self):  
+        current_time = input('Current time is 1000 do yo want to change it? Enter y/n:  ')  
+        self.time_now= current_time
+        if current_time == 'y':
+            current_time = input ('What time is it? ')
+        else:
+            current_time = 1000    
 
-def set_alarm_time(self):
-    wakeup_time = input('What time do you want to wake up? ')
-    self.alarm_time = wakeup_time   
-    if alarm_set == False:
-        print('Alarm is off! ')
-        response=input('Do you want to turm it on? Enter y/n: ')
-    if response == 'y':
-        self.alarm_om = True
-    else:
-        self.alarm_on = False    
+    def set_alarm_time(self):
+        wakeup_time = input('What time do you want to wake up? ')
+        self.alarm_time = wakeup_time   
+          
 
 
 
 
-def alarm_set(self):
-    if self.alarm_on == True:
-        print('Alarm is already on! ') 
-    else :
-        self.alarm_on == False            
+    def alarm_set(self):
+        if self.alarm_on == False:
+         print('Alarm is off! ')
+         response=input('Do you want to turm it on? Enter y/n: ')
+         if response == 'y':
+             self.alarm_om = True
+             print ("Alarm is on!")
+         else:
+             self.alarm_on = False  
